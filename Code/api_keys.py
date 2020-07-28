eac8f1b90fa9831ca208284c0635418582db9492
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-07-28 12:42:40 -0400

    close to final commit

--- Code/api_keys.py
+++ Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "c84879c9d7b2f78e9b03f87b4c14f86d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBPBOW79VkwXHZiE5dKsTopqa5DAJdm5g0"
